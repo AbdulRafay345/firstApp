d2393d72d3562bc284f438a36862ebef50618152
Abdul Rafay <abdrafay345@gmail.com>
2024-12-29 07:55:19 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function App () {
+    return (
+      <View>
+        <Text style={{color:"white"}}> Tadii v </Text>
+        <Text style={{color:"white"}}> Tadii v </Text>
+      </View>
+    )
+}
